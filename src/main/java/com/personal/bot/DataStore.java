40d4fc8ad8fcf4ae0aaf5f4bccebc81a4d8783f8
Mohammed Shanto <almohammed2762@gmail.com>
2026-01-21 18:54:54 +0600

    Add DataStore class for BotData persistence
    Implement load and save methods for BotData using Gson.

--- src/main/java/com/personal/bot/DataStore.java
+++ src/main/java/com/personal/bot/DataStore.java
@@ -1,1 +1,21 @@
+import com.google.gson.Gson;
+import java.io.*;
 
+public class DataStore {
+    private static final String FILE = "data.json";
+    private static final Gson gson = new Gson();
+
+    public static BotData load() {
+        try {
+            return gson.fromJson(new FileReader(FILE), BotData.class);
+        } catch (Exception e) {
+            return new BotData();
+        }
+    }
+
+    public static void save(BotData data) {
+        try (FileWriter writer = new FileWriter(FILE)) {
+            gson.toJson(data, writer);
+        } catch (Exception ignored) {}
+    }
+}
