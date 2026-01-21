0175dc951e67808946c8d63446b683a33e0302d3
Mohammed Shanto <almohammed2762@gmail.com>
2026-01-21 19:27:48 +0600

    Update Main.java

--- src/main/java/com/personal/bot/Main.java
+++ src/main/java/com/personal/bot/Main.java
@@ -1,1 +1,9 @@
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(new LedgerBot());
+    }
+}
